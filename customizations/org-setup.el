74618f1351b53c36ce7b489427044ffa3ec29137
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-11-02 13:50:07 +0100

    Org shortcuts

--- customizations/org-setup.el
+++ customizations/org-setup.el
@@ -3,3 +3,8 @@
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 (setq org-default-notes-file "~/notes/notes.org")
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cn" (lambda () (interactive) (find-file "~/notes/notes.org")))
